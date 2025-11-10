599a789269459b00388abbe4452d8fba03f9ca78
Emanuele Zanusso <afteremanuele@gmail.com>
2025-11-10 23:46:39 +0100

    Ultime modifiche

--- 11:11:2025/imgen.C
+++ 11:11:2025/imgen.C
@@ -30,7 +30,21 @@ void imgen(string name_file)
         h->SetTitle("Recoil mass of #gamma");
         tela->SaveAs("images/mRecoil_gamma_Jpsi.pdf");
         
+        tree->Draw("vpho_r_pRecoilTheta-nbar_theta>>h(40,-0.4,0.4)","","colz");
+        h = (TH1*)gDirectory->Get("h");
+        h->SetXTitle("#Delta#theta [rad]");
+        h->SetYTitle("counts []");
+        h->SetTitle("#theta difference");
+        tela->SaveAs("images/theta_diff_Jpsi.pdf");
+        
         gStyle->SetOptStat(0);
+        
+        tree->Draw("vpho_r_pRecoilTheta:nbar_theta>>h(40,0,3.2,40,0,3.2)","","colz");
+        h = (TH2*)gDirectory->Get("h");
+        h->SetXTitle("#bar{n} #theta [rad]");
+        h->SetYTitle("recoil #theta [rad]");
+        h->SetTitle("#theta correlation");
+        tela->SaveAs("images/theta_corr_Jpsi.pdf");
         
         
         myf.Close();
@@ -57,17 +71,31 @@ void imgen(string name_file)
         TH1* h = (TH1*)gDirectory->Get("h");
         h->SetXTitle("m_{recoil} [GeV]");
         h->SetYTitle("counts []");
-        h->SetTitle("Recoil mass of p+, #pi-, #gamma");
+        h->SetTitle("Recoil mass of p+, #pi-, #gamma kin");
         tela->SaveAs("images/mRecoil_Jpsi_kin.pdf");
         
         tree->Draw("gamma_mRecoil>>h(100,0,11)");
         h = (TH1*)gDirectory->Get("h");
         h->SetXTitle("m_{recoil} [GeV]");
         h->SetYTitle("counts []");
-        h->SetTitle("Recoil mass of #gamma");
+        h->SetTitle("Recoil mass of #gamma kin");
         tela->SaveAs("images/mRecoil_gamma_Jpsi_kin.pdf");
         
+        tree->Draw("vpho_r_pRecoilTheta-nbar_theta>>h(40,-0.4,0.4)","","colz");
+        h = (TH1*)gDirectory->Get("h");
+        h->SetXTitle("#Delta#theta [rad]");
+        h->SetYTitle("counts []");
+        h->SetTitle("#theta difference kin");
+        tela->SaveAs("images/theta_diff_Jpsi_kin.pdf");
+        
         gStyle->SetOptStat(0);
+        
+        tree->Draw("vpho_r_pRecoilTheta:nbar_theta>>h(40,0,3.2,40,0,3.2)","","colz");
+        h = (TH2*)gDirectory->Get("h");
+        h->SetXTitle("#bar{n} #theta [rad]");
+        h->SetYTitle("recoil #theta [rad]");
+        h->SetTitle("#theta correlation kin");
+        tela->SaveAs("images/theta_corr_Jpsi_kin.pdf");
         
         
         myf.Close();
